fd5838d5fee21fbb61ca0a18c5cdb454d0c98071
code-server <nooyod@gmail.com>
2021-10-26 18:56:32 +0000

    V3 css red add, utf-8 fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function dosearch() {
     var sf=document.searchform;
-    var submitto = sf.sengines.options[sf.sengines.selectedIndex].value + escape(sf.searchterms.value);
+    var submitto = sf.sengines.options[sf.sengines.selectedIndex].value + sf.searchterms.value;
     window.location.href = submitto;
     return false;
     }
